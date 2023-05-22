d412c995f0cb95b2348abc62b1b77f0c277b7355
kyrieCCC <1186624948@qq.com>
2023-05-22 09:28:57 +0800

    feat: 引入了less

--- src/container/Index/index.jsx
+++ src/container/Index/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Button } from 'zarm'
+import s from './style.module.less'
 
 export default function Index() {
-    return (<div>
+    return (<div className={s.index}>
         Index Page
+        <span>我的样式出现了变化</span>
         <Button theme="primary"> 按钮 </Button>
     </div>
     )
