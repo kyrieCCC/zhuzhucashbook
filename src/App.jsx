403e496a985b647aa83cd8743ef91a4403bbb721
kyrieCCC <1186624948@qq.com>
2023-05-22 08:52:41 +0800

    feat: 按需引入zarmUI库

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import {
   Route,
   Routes
 } from "react-router-dom"
+import { ConfigProvider } from 'zarm';
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
+import 'zarm/dist/zarm.css';
 import routes from '../src/router'
 function App() {
   return <Router>
+    <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
     <Routes>
       {
         routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component/>} />)
       }
-    </Routes>
+      </Routes>
+      </ConfigProvider>
   </Router>
 }
 
--- src/container/Index/index.jsx
+++ src/container/Index/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Button } from 'zarm'
 
 export default function Index() {
     return (<div>
         Index Page
-    </div>)
+        <Button theme="primary"> 按钮 </Button>
+    </div>
+    )
 }
