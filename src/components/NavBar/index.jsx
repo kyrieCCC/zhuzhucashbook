f0f53062ab63db8e471685881e33d693d6940150
kyrieCCC <1186624948@qq.com>
2023-05-23 18:11:26 +0800

    feat: 底部导航栏的图标

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { TabBar } from "zarm";
 import { useNavigate } from "react-router-dom";
 import s from './style.module.less';
+import CustomIcon from '../CustomIcon';
 
 const NavBar = ({ showNav }) => {
     const [activeKey, setActiveKey] = useState('/');
@@ -18,14 +19,17 @@ const NavBar = ({ showNav }) => {
           <TabBar.Item
             itemKey="/"
             title="账单"
+            icon={<CustomIcon type="zhangdan" />}
           />
           <TabBar.Item
             itemKey="/data"
-            title="统计"
+          title="统计"
+          icon={<CustomIcon type="tongji" />}
           />
           <TabBar.Item
             itemKey="/user"
-            title="我的"
+          title="我的"
+          icon={<CustomIcon type="wode" />}
           />
         </TabBar>
       );
