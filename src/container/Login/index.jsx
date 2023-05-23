f7aa0ed225ea33341ae293af94e85ad7ed0a2f29
kyrieCCC <1186624948@qq.com>
2023-05-23 22:21:04 +0800

    feat: 添加login标签页

---
+++ src/container/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './style.module.less'
+
+const Login = () => {
+    return (
+        <div>
+            注册页面
+        </div>
+    )
+}
+
+export default Login
