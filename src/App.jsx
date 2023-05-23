7784cba3fb4146d091fd7523dac405bd4f6e2496
kyrieCCC <1186624948@qq.com>
2023-05-23 15:05:31 +0800

    feat: 配置相关路由信息

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { ConfigProvider } from 'zarm';
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
 import 'zarm/dist/zarm.css';
 import routes from '@/router'
+import NavBar from '@/components/NavBar'
+
+
 function App() {
   return <Router>
     <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
@@ -16,7 +19,9 @@ function App() {
         routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component/>} />)
       }
       </Routes>
-      </ConfigProvider>
+      <NavBar showNav={showNav } />
+    </ConfigProvider>
+    <NavBar showNav={ true } />
   </Router>
 }
 
