c50593c245c9a7e9d1e71726e4e5b87ff111c5c8
kyrieCCC <1186624948@qq.com>
2023-05-23 15:39:28 +0800

    配置router信息

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { TabBar } from "zarm";
 import { useNavigate } from "react-router-dom";
-import s from './style.module.less';
+// import s from './style.module.less';
 
 const NavBar = ({ showNav }) => {
     const [activeKey, setActiveKey] = useState('/');
@@ -14,7 +14,7 @@ const NavBar = ({ showNav }) => {
     }
 
     return (
-        <TabBar visible={showNav} className={s.tab} activeKey={activeKey} onChange={changeTab}>
+        <TabBar visible={props.showNav} className={s.tab} activeKey={activeKey} onChange={changeTab}>
           <TabBar.Item
             itemKey="/"
             title="账单"
