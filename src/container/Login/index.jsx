3c855749fc7c27e975f2be1160be40d1f72e75e6
kyrieCCC <1186624948@qq.com>
2023-05-25 16:58:33 +0800

    feat: 添加二维码图片

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import s from "./style.module.less";
 import { Input, Cell, Button, Checkbox } from "zarm";
+import Captcha from 'react-captcha-code';
 
 const Login = () => {
+
+  const [username, setUsername] = useState(''); // 账号
+  const [password, setPassword] = useState(''); // 密码
+  const [verify, setVerify] = useState(''); // 验证码
+  const [captcha, setCaptcha] = useState('');
+
+  // 检测验证码变化
+  const handleChange = useCallback((captcha) => {
+    console.log('captcha:', captcha);
+    setCaptcha(captcha)
+  }, []);
 
   return (
     <div className={s.auth}>
@@ -12,13 +24,14 @@ const Login = () => {
       </div>
       <div className={s.form}>
         <Cell title="账号">
-          <Input clearable type="text" placeholder="请输入" />
+          <Input clearable type="text" placeholder="请输入" onChange={(value) => setUsername(value)} />
         </Cell>
         <Cell title="密码">
-          <Input clearable type="password" placeholder="请输入" />
+          <Input clearable type="password" placeholder="请输入" onChange={(value) => setPassword(value)} />
         </Cell>
         <Cell title="验证码">
-          <Input clearable type="text" placeholder="请输入验证码" />
+          <Input clearable type="text" placeholder="请输入验证码" onChange={(value) => setVerify(value)} />
+          <Captcha charNum={4} onChange={handleChange} />
         </Cell>
       </div>
       <div className={s.operation}>
