969686e07d0ad35491fa2c8efa9eef6d23292131
kyrieCCC <1186624948@qq.com>
2023-05-28 13:54:15 +0800

    feat: 统计页面的饼图绘制

--- src/container/Data/index.jsx
+++ src/container/Data/index.jsx
@@ -8,6 +8,8 @@ import PopupDate from '@/components/PopupDate';
 
 import s from './style.module.less';
 
+let proportionChart = null // 存放echart的实例对象
+
 const Data = () => {
     const monthRef = useRef();
     const [currentMonth, setCurrentMonth] = useState(dayjs().format('YYYY-MM'));
@@ -16,9 +18,14 @@ const Data = () => {
     const [totalIncome, setTotalIncome] = useState(0); // 总收入
     const [expenseData, setExpenseData] = useState([]); // 支出数据
     const [incomeData, setIncomeData] = useState([]); // 收入数据
+    const [pieType, setPieType] = useState('expense'); // 饼图的「收入」和「支出」控制
 
     useEffect(() => {
-        getData()
+        getData();
+        return () => {
+            // 每次组件卸载的时候，需要释放图表实例。clear 只是将其清空不会释放。
+            proportionChart.dispose();
+        };
     }, [currentMonth]);
 
     // 获取数据详情
@@ -34,6 +41,9 @@ const Data = () => {
         const income_data = data.total_data.filter(item => item.pay_type == 2).sort((a, b) => b.number - a.number); // 过滤出账单类型为收入的项
         setExpenseData(expense_data);
         setIncomeData(income_data);
+
+        // 绘制饼图
+        setPieChart(pieType == 'expense' ? expense_data : income_data);
     };
 
     // 月份弹窗开关
@@ -51,6 +61,53 @@ const Data = () => {
         setTotalType(type)
     };
 
+    // 切换饼图收支类型
+    const changePieType = (type) => {
+        setPieType(type);
+        // 重绘饼图
+        setPieChart(type == 'expense' ? expenseData : incomeData);
+    };
+
+    // 绘制饼图方法
+    const setPieChart = (data) => {
+        if (window.echarts) {
+            // 初始化饼图，返回实例。
+            proportionChart = echarts.init(document.getElementById('proportion'));
+            proportionChart.setOption({
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{a} <br/>{b} : {c} ({d}%)'
+                },
+                // 图例
+                legend: {
+                    data: data.map(item => item.type_name)
+                },
+                series: [
+                    {
+                        name: '支出',
+                        type: 'pie',
+                        radius: '55%',
+                        data: data.map(item => {
+                            return {
+                                value: item.number,
+                                name: item.type_name
+                            }
+                        },
+                        ),
+                        emphasis: {
+                            itemStyle: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        },
+                        // roseType: 'area' // 可以指定南丁格尔玫瑰图
+                    }
+                ]
+            })
+        };
+    };
+
     return (
         <div className={s.data}>
             <div className={s.total}>
@@ -59,8 +116,8 @@ const Data = () => {
                     <Icon className={s.date} type="date" />
                 </div>
                 <div className={s.title}>共支出</div>
-                <div className={s.expense}>¥{ totalExpense }</div>
-                <div className={s.income}>共收入￥{totalIncome}</div>
+                <div className={s.expense}>¥{totalExpense}</div>
+                <div className={s.income}>共收入¥{totalIncome}</div>
             </div>
             <div className={s.structure}>
                 <div className={s.head}>
@@ -96,10 +153,21 @@ const Data = () => {
                         </div>)
                     }
                 </div>
+                <div className={s.proportion}>
+                    <div className={s.head}>
+                        <span className={s.title}>收支构成</span>
+                        <div className={s.tab}>
+                            <span onClick={() => changePieType('expense')} className={cx({ [s.expense]: true, [s.active]: pieType == 'expense' })}>支出</span>
+                            <span onClick={() => changePieType('income')} className={cx({ [s.income]: true, [s.active]: pieType == 'income' })}>收入</span>
+                        </div>
+                    </div>
+                    <div id="proportion"></div>
+                </div>
             </div>
             <PopupDate ref={monthRef} mode="month" onSelect={selectMonth} />
         </div>
     );
 }
+
 
 export default Data
